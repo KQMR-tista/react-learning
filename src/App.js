5b88efbe1b2bb3d5ef9ee1991c7733dda46aca62
Ajith Keerikkattil <ajithkeerikkattil@ajiths-air.myfiosgateway.com>
2024-09-02 10:42:24 -0400

    useEffect

--- src/App.js
+++ src/App.js
@@ -30,8 +30,8 @@ function App() {
             <UseReduceHook />
             <UseStateHook/>
             <UseMemoHook />
-            <UseEffectHook/>
             <UseRefHook/>
+            <UseEffectHook/>
 
             {/*<RenderProps render={PropComponentFunction}></RenderProps>*/}
 
--- src/interviewPrepHooks/UseEffectHook.js
+++ src/interviewPrepHooks/UseEffectHook.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
+/**
+ * Class components have lifecycle hooks: componentDidMount, componentDidUpdate, and componentWillUnmount.
+ * Functional components: The above lifecycle hooks are all handled by useEffect hook
+ */
+
+
+/**
+ * Syntax
+ * import { useEffect } from 'react';
+ * useEffect(function callMeOnMount(){
+ *     // do something
+ * }, []);
+ */
 function UseEffectHook(props) {
+    let [val, setVal] = useState(0);
+    let [anotherVal, setAnotherVal] = useState(0);
 
-    sessionStorage.setItem('ajith', 'ajith keerikkattil');
-    console.log(sessionStorage.getItem('ajith'))
+    // When no dependency array is specified. Runs everytime the component is rendered.
+    useEffect(() => {
+        console.log('run on every render')
+    })
+
+    // When empty dependency array is specified. Runs only once the first time the component is mounted.
+    useEffect(() => {
+        console.log('run on first render only')
+    }, []);
+
+    // When dependency array has variables. Runs only when any of the variables in the dependency array changes.
+    useEffect(() => {
+        console.log('run on \'val\' change only')
+    }, [val]);
+
+    // When a return callback function is specified. The code inside it is run when the component unmounts.
+    // Ideal place for cleanup
+    useEffect(() => {
+        return () => {
+            setVal(0);
+            setAnotherVal(0);
+            console.log(val);
+            console.log(anotherVal);
+        };
+    }, []);
+
     return (
-        <div></div>
+        <>
+            <h1> Learning UseEffect Hook</h1>
+            <button onClick={() => setVal(val++)}>Click me (Val)</button>
+            <button onClick={() => setAnotherVal(val++)}>Click me (AnotherVal)</button>
+        </>
     );
 }
 
